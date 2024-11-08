726f66f239b906d620f62a489d97377ba46e16e9
Mahdirz <mahdirazmkhah45@gmail.com>
2024-11-08 15:24:50 +0330

    feat: add blog update path

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='blogs'),
     path('new/', views.BlogCreateView.as_view(), name='new_blog'),
+    path('<int:pk>', views.UpdateView.as_view(), name='update_blog'),
 ]
