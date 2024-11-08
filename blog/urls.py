b19ee4cf16292b48f69126342119ca91ce57f4d1
Mahdirz <mahdirazmkhah45@gmail.com>
2024-11-08 15:33:22 +0330

    fix: correct git update path

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='blogs'),
     path('new/', views.BlogCreateView.as_view(), name='new_blog'),
-    path('<int:pk>', views.UpdateView.as_view(), name='update_blog'),
+    path('<int:pk>/edit/', views.BlogUpdateView.as_view(), name='update_blog'),
 ]
