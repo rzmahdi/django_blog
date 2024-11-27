3cc6a1b1985d834dd7152f827f3ff2bb7febb69d
Mahdirz <mahdirazmkhah45@gmail.com>
2024-11-27 21:26:18 +0330

    feat: add CustomUserModel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUserModel(AbstractUser):
+    email = models.EmailField(("example@gmail.com"), null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
