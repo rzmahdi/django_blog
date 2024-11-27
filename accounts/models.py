4c20e5e0f16e27e5d2064c88760c3a921d389a91
Mahdirz <mahdirazmkhah45@gmail.com>
2024-11-28 00:36:28 +0330

    delete CustomUserModel email default value

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUserModel(AbstractUser):
-    email = models.EmailField(("example@gmail.com"), null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
