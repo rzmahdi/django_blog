60b1fa547eb5808fd61e6c7c736c923ae5132b31
Mahdirz <mahdirazmkhah45@gmail.com>
2024-11-27 21:43:27 +0330

    feat: update Blog model to use settings.AUTH_USER_MODEL for author

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     creation_time = models.DateTimeField(auto_now_add=True)
     visable = models.BooleanField(default=True)
 
