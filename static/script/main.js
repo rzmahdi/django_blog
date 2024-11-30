a17b1ee546baf8cba2043bb20436e0f5820f7582
Mahdirz <mahdirazmkhah45@gmail.com>
2024-12-01 00:11:23 +0330

    refactor: change add animation to blogs drop-down

--- static/script/main.js
+++ static/script/main.js
@@ -2,31 +2,23 @@ function toggleMenu(blogId) {
     const menus = document.querySelectorAll('.menu');
     menus.forEach(menu => {
         if (menu.id !== `menu-${blogId}`) {
-            menu.style.display = "none";
+            menu.classList.remove("show");
         }
     });
 
     const menu = document.getElementById(`menu-${blogId}`);
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
+    if (menu.classList.contains("show")) {
+        menu.classList.remove("show");
     } else {
-        menu.style.display = "block";
+        menu.classList.add("show");
     }
-}
-
-function toggleCancel(blogId){
-    const delete_menu = document.getElementById(`delete-${blogId}`);
-    const menu = document.getElementById(`menu-${blogId}`);
-    
-    delete_menu.style.display = "none";
-    menu.style.display = "none";
 }
 
 window.onclick = function(event){
     if(!event.target.matches('.menu-btn')){
         const menus = document.querySelectorAll('.menu');
         menus.forEach(menu => {
-            menu.style.display = "none";
+            menu.classList.remove("show");
         })
     }
 }
