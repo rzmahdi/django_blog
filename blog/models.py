3ad45015904a11d092af1b7fb3cf41cbd726493a
Mahdirz <mahdirazmkhah45@gmail.com>
2024-12-02 20:17:25 +0330

    feat: add Comment model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
@@ -10,3 +11,15 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    comment = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author} - {self.comment[:10]}"
+    
+    def get_absolute_url(self):
+        return reverse("blogs")
