07fa32d9897e376ee6aad9e72c23405fa48dc582
Mahdirz <mahdirazmkhah45@gmail.com>
2024-12-03 10:25:57 +0330

    feat: add extra to manage default comment places

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,10 @@ from .models import Blog, Comment
 
 class CommentInline(admin.StackedInline):
     model = Comment
+    extra = 0
 
 class BlogAdmin(admin.ModelAdmin):
     inlines = [CommentInline]
-
 
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(Comment)
