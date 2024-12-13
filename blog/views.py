35f4ab75c2610c8b679678a1980bf8cf9023f45b
Mahdirz <mahdirazmkhah45@gmail.com>
2024-12-14 00:44:40 +0330

    feat: add comment dependments in BlogDetailView

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Blog
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import CommentForm
+from django.urls import reverse
 
 
 class BlogListView(ListView):
@@ -15,10 +16,32 @@ class BlogListView(ListView):
         return Blog.objects.all().order_by('-creation_time')
     
 
-class BlogDetailView(DetailView):
+class BlogDetailView(FormMixin, DetailView):
     model = Blog
     template_name = "blog/detail.html"
     context_object_name = "blog"
+    form_class = CommentForm
+
+
+    def get_success_url(self):
+        return reverse("detail_blog", kwargs={"pk": self.object.id})
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogDetailView, self).get_context_data(**kwargs)
+        context["form"] = CommentForm(initial={"blog": self.object, "author": self.request.user})
+        return context
+    
+    def post(self, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            pass
+
+    def form_valid(self, form):
+        form.save()
+        return super(BlogDetailView, self).form_valid(form)
 
 
 class BlogCreateView(LoginRequiredMixin, CreateView):
