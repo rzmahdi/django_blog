8a2509927f6ed44642a3042347dc493a694d99b7
Mahdirz <mahdirazmkhah45@gmail.com>
2024-12-13 22:37:47 +0330

    feat: add detail url

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='blogs'),
     path('new/', views.BlogCreateView.as_view(), name='new_blog'),
+    path('<int:pk>/', views.BlogDetailView.as_view(), name='detail_blog'),
     path('<int:pk>/edit/', views.BlogUpdateView.as_view(), name='update_blog'),
     path('<int:pk>/delete/', views.BlogDeleteView.as_view(), name='delete_blog'),
 ]
